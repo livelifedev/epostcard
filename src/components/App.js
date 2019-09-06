3a4184a6929f87f2c917dcdcbb848e31ef6c88c6
John <johnrubio93@gmail.com>
2019-09-06 13:42:08 +1000

    first commit, created index, App and Postcard component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostcardView from "../pages/PostcardView";
+
+const App = () => {
+  return (
+    <>
+      <PostcardView />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/PostcardView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostcardView = () => {
+  return (
+    <div>
+      <h1>Mail your Web Postcard!</h1>
+    </div>
+  );
+};
+
+export default PostcardView;

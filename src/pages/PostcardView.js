f7f337ab5142e3e7d490ba4964c136081be81937
John <johnrubio93@gmail.com>
2019-09-07 08:05:45 +1000

    added sendee hook

--- src/pages/PostcardView.js
+++ src/pages/PostcardView.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PostcardView = () => {
+  const [sendee, setSendee] = useState("");
+
   return (
     <div>
       <h1>Mail your Web Postcard!</h1>
+      <form>
+        <label htmlFor="sendTo">
+          To:
+          <input
+            type="text"
+            name="sendTo"
+            id="sendTo"
+            placeholder="John Doe"
+            value={sendee}
+            onChange={e => setSendee(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
